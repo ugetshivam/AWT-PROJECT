58036a5a790360e225631452ef7ec256165c09d1
Shivam <shiv.rawat594@gmail.com>
2021-05-19 16:30:30 +0530

    added social footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import CreateAcct from './Components/CreateAcct';
+import CreateAcct from "./Components/CreateAcct";
 import LeaveArev from "./Components/LeaveArev";
 import Sign from "./Components/Sign";
 import Home from "./Components/Home";
+import Social from "./Components/Social";
 import Navigation from "./navbar";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
 function App() {
   return (
     <div>
-      <Navigation />
-      <Switch>
-        <Route path="/createaccount" component={CreateAcct} />
-        <Route exact path="/" component={Home} />
-        <Route path="/sign" component={Sign} />
-        <Route path="/leavearev" component={LeaveArev} />
-      </Switch>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/createaccount" component={CreateAcct} />
+          <Route exact path="/" component={Home} />
+          <Route path="/sign" component={Sign} />
+          <Route path="/leavearev" component={LeaveArev} />
+        </Switch>
+      </div>
+      <Social />
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Cards from "./Cards";
+
 function Home() {
   return (
     <div>
+      <h1 style={{ marginTop: "70px", textAlign: "center" }}>Editor's Pick</h1>
+      <hr />
       <Cards />
+      <hr />
     </div>
   );
 }
